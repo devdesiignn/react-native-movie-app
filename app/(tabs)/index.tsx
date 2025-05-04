b2e243bf3f597705dde6c527e6730a7e7a19f4bd
Muiz Haruna <muizharuna55@gmail.com>
2025-05-04 09:33:36 +0100

    feat: added screens and route

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex flex-1 justify-center items-center">
+      <Text className="text-4xl font-bold text-purple-500">Welcome</Text>
+    </View>
+  );
+}
