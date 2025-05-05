cda7d720a57489b9b0ebb86b7cc50a417d37ef79
Muiz Haruna <muizharuna55@gmail.com>
2025-05-05 15:25:35 +0100

    feat: set up useFetch

---
+++ hooks/useFetch.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState, useCallback } from "react";
+
+const useFetch = <T>(fetchFunction: () => Promise<T>, autoFetch = true) => {
+  const [data, setData] = useState<T | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = useCallback(async () => {
+    try {
+      setLoading(true);
+      setError(null);
+
+      const result = await fetchFunction();
+
+      setData(result);
+    } catch (error) {
+      setError(error instanceof Error ? error : new Error("An Error Occured!"));
+    } finally {
+      setLoading(false);
+    }
+  }, [fetchFunction]);
+
+  const reset = () => {
+    setData(null);
+    setLoading(false);
+    setError(null);
+  };
+
+  useEffect(() => {
+    if (autoFetch) fetchData();
+  }, [autoFetch, fetchData]);
+
+  return { data, loading, error, refetch: fetchData, reset };
+};
+
+export default useFetch;
