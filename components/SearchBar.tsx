46a474fe30c08c667afb7930c4ee449956d1acbf
Muiz Haruna <muizharuna55@gmail.com>
2025-05-05 09:59:07 +0100

    feat: added searchbar to home screen

---
+++ components/SearchBar.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Image, TextInput, View } from "react-native";
+
+import { icons } from "@/constants/icons";
+
+type SearchBarProps = {
+  onPress?: () => void;
+  placeHolder: string;
+};
+
+const SearchBar = ({ onPress, placeHolder }: SearchBarProps) => {
+  return (
+    <View className="flex flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+      <Image source={icons.search} className="size-5" resizeMode="contain" tintColor="#ab8bff" />
+
+      <TextInput
+        placeholder={placeHolder}
+        placeholderTextColor="#ab8bff"
+        value=""
+        onPress={onPress}
+        onChangeText={() => {}}
+        className="flex-1 ml-2 text-white"
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
